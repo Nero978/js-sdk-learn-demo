fb858a3487dbd06e8c36145675b2eae2ac24d784
Nero978 <zhumengfeiyang@qq.com>
2024-10-17 16:03:42 +0800

    feat: 更新依赖版本
    更新了"@lark-base-open/js-sdk"的版本为"0.4.0"，以及其他一些依赖的版本更新。

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import { bitable, IAttachmentField } from '@lark-base-open/js-sdk';
+import { bitable, ICurrencyField } from '@lark-base-open/js-sdk';
 import { Alert, AlertProps } from 'antd';
+import { Button } from 'antd';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <LoadApp/>
+    <LoadApp />
   </React.StrictMode>
 )
 
@@ -15,14 +16,37 @@ function LoadApp() {
   useEffect(() => {
     const fn = async () => {
       const table = await bitable.base.getActiveTable();
-      const tableName = await table.getName();
-      setInfo(`The table Name is ${tableName}`);
+      const view = await table.getActiveView();
+      const viewName = await view.getName();
+      setInfo(`当前视图为 ${viewName}`);
       setAlertType('success');
     };
     fn();
   }, []);
 
+  const getCsvContent = async () => {
+    const table = await bitable.base.getActiveTable();
+    const view = await table.getActiveView();
+    const recordIdList = await view.getVisibleRecordIdList();
+    const fieldIdList = await view.getVisibleFieldIdList();
+    let data = [];
+    for(const recordId of recordIdList) {
+      let recordValue = [];
+      for(const fieldId of fieldIdList) {
+        const field = await table.getFieldById(fieldId);
+        const cell = await field.getCellString(recordId);
+        recordValue.push(cell);
+      }
+      data.push(recordValue);
+    }
+    console.log(data);
+  }
+
   return <div>
     <Alert message={info} type={alertType} />
+    <div style={{ margin: 10 }}>
+      <div>下载 csv</div>
+      <Button style={{ marginLeft: 10 }} onClick={getCsvContent}>下载</Button>
+    </div>
   </div>
 }
